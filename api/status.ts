639d543d39b9fb5abe65be29f7a0a07c4f48ae0f
EnderDev <kieran@dothq.org>
2023-02-15 22:23:27 +0000

    add patch version support

--- api/status.ts
+++ api/status.ts
@@ -2,6 +2,8 @@ import type { VercelRequest, VercelResponse } from "@vercel/node";
 import { badge } from "../src/badge";
 import { getDotFFVersion, getMozFFVersion } from "../src/versions";
 
+const plural = (condition: boolean, name?: string) => condition == true ? (name || "s") : "";
+
 export default async function (req: VercelRequest, res: VercelResponse) {
     const dotFFVersion = await getDotFFVersion();
     const mozFFVersion = await getMozFFVersion();
@@ -11,11 +13,14 @@ export default async function (req: VercelRequest, res: VercelResponse) {
 
     const majorsBehind = ffVersion[0] - dotVersion[0];
     const minorsBehind = ffVersion[1] - dotVersion[1];
+    const patchesBehind = ffVersion[2] ? ffVersion[2] - (dotVersion[2] || 0) : 0;
 
     if (majorsBehind >= 1) {
-        return badge(res, "upstream", `${majorsBehind} major versions behind`, "red");
+        return badge(res, "upstream", `${majorsBehind} major version${plural(majorsBehind > 1)} behind`, "red");
+    } else if (minorsBehind >= 1 && patchesBehind >= 1) {
+        return badge(res, "upstream", `${minorsBehind} minor version${plural(majorsBehind > 1)} and ${patchesBehind} patch${plural(patchesBehind > 1, "es")} behind`, "yellow");
     } else if (minorsBehind >= 1) {
-        return badge(res, "upstream", `${minorsBehind} minor versions behind`, "yellow");
+        return badge(res, "upstream", `${minorsBehind} minor version${plural(majorsBehind > 1)} behind`, "yellow");
     } else {
         return badge(res, "upstream", `0 versions behind`, "#238636");
     }
